391a0738e42c0eb3edef5e4fee4897bf8a016542
joeneedum <joeneedum@gmail.com>
2022-03-23 19:49:26 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ export default function App() {
     <Jumbotron.Container>
       {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
-          <p>Hello</p>
+          <Jumbotron.Pane>
+            <Jumbotron.Title><p>{item.title}</p></Jumbotron.Title>
+            <Jumbotron.SubTitle><p>{item.subTitle}</p></Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
         </Jumbotron>
       ))}
     </Jumbotron.Container>
