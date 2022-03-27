de349923b1f7e2065f84985b42d1f4d636b14ac6
joeneedum <joeneedum@gmail.com>
2022-03-27 19:40:55 -0400

    commit

---
+++ src/components/feature/styles/feature.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/macro'
+
+export const Container = styled.div`
+`;
+
+export const Title = styled.div`
+    color: white;
+    max-width: 640px;
+    
+`;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import JumbotronContainer from "../containers/jumbotron";
+import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from '../containers/header';
+import { OptForm } from '../components';
 
 export default function home() {
     return (
         <>
+            <HeaderContainer />
             <JumbotronContainer/>
             <FaqsContainer />
             <FooterContainer />
