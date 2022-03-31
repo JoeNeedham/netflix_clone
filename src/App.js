4828fd166b6a3ad661818e208b482307ee61e071
joeneedum <joeneedum@gmail.com>
2022-03-31 17:39:44 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import * as ROUTES from './constants/routes'
 import { Home, Signin, Signup, Browse } from "./pages";
+import { IsUserRedirect } from "./helpers/routes";
 
 export default function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route exact path={ROUTES.HOME} element={<Home />} />
@@ -13,5 +15,7 @@ export default function App() {
         <Route exact path={ROUTES.BROWSE} element={<Browse />} />
       </Routes>
     </BrowserRouter>
+    
+    </>
   );
 }
