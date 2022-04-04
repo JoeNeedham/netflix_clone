f1f6423bca3ba085c0734d3e630d94909d583515
joeneedum <joeneedum@gmail.com>
2022-04-03 22:29:21 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<ProtectedRoutes />}>
-          <Route exact path={ROUTES.HOME} element={<Home />} />
           <Route exact path={ROUTES.SIGN_IN} element={<Signin />} />
         </Route>
-          <Route exact path={ROUTES.SIGN_UP} element={<Signup />} />
-          <Route exact path={ROUTES.BROWSE} element={<Browse />} />
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path={ROUTES.SIGN_UP} element={<Signup />} />
+        <Route exact path={ROUTES.BROWSE} element={<Browse />} />
       </Routes>
     </BrowserRouter>
     
