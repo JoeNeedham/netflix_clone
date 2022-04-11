25f31c9a6636b55f0c21d11cb83fd3dd5b501305
joeneedum <joeneedum@gmail.com>
2022-04-10 23:29:21 -0400

    commit

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -130,6 +130,7 @@ export const PlayButton = styled.button`
 box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
 background-color: #e6e6e6;
 color: #000;
+font-weight: bold;
 border-width: 0;
 padding: 10px 20px;
 border-radius: 5px;
